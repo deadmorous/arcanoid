9939fd552f0823dd7b054d64d8876bc9e4b8e899
Chechen13340 <38070832+Chechen13340@users.noreply.github.com>
2018-05-08 14:26:39 +0300

    Add files via upload

---
+++ arcanoid-ball.js
@@ -0,0 +1,32 @@
+(function() {
+    function Ball(gameState)
+    {
+        this.speed = [0, -0.01,0.5]
+        this.pos = [0.55, 0.965]
+        this.radius = 0.03
+        arcanoid.GamePainter.created.handle(function(painter) {
+            gameState.ballMoved.handle(
+                positionBall.bind(painter))
+        })
+    }
+
+    function positionBall()
+    {
+        var ball = this.state.ball
+        var pos = ball.pos
+        this.ball.offset(this.toScreen(pos[0]-ball.radius, pos[1]-ball.radius))
+    }
+
+    function drawBall()
+    {
+        this.ball = $('<div>')
+            .addClass('ball')
+            .text('O')
+            .appendTo(this.container)
+        positionBall.call(this)
+    }
+
+    arcanoid.GamePainter.addItemPainter(drawBall)
+
+    arcanoid.Ball = Ball
+})()
--- arcanoid-game-state.js
+++ arcanoid-game-state.js
@@ -11,15 +11,22 @@
         ];
         // Important: Events are created first!
         this.paddleMoved = new arcanoid.Event
+        this.ballMoved = new arcanoid.Event
 
         this.paddle = new arcanoid.Paddle(this)
         this.brick = new arcanoid.Bricks(bricks_list)
+        this.ball = new arcanoid.Ball(this)
     }
     GameState.prototype.next = function(dt)
     {
         if (this.paddle.speed != 0) {
             this.paddle.pos += this.paddle.speed*dt
             this.paddleMoved.raise()
+        }
+        if (!(this.ball.speed[0] === 0 && this.ball.speed[1] === 0)) {
+            this.ball.pos[0] += this.ball.speed[0]*dt
+            this.ball.pos[1] += this.ball.speed[1]*dt
+            this.ballMoved.raise()
         }
     }
     arcanoid.GameState = GameState
