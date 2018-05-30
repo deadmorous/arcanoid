0eacc31cd1fdc2988274309b81ae0e10a230e60d
Anna <nycik_pucik@mail.ru>
2018-05-30 12:24:27 +0300

    work

--- arcanoid-game-state.js
+++ arcanoid-game-state.js
@@ -10,21 +10,52 @@
         this.paddle = new arcanoid.Paddle(this)
         arcanoid.makeRound(this,roundNumber)
         this.ball = new arcanoid.Ball(this)
-        this.score = new arcanoid.Score(this)
     }
     GameState.prototype.next = function(dt)
     {
         if (this.paddle.speed != 0) {
             this.paddle.pos += this.paddle.speed*dt
+
+            //Ширина ракетки.
+            var paddle_width =  $('.paddle').width() / $('#game').width();
+            //Предотвращение выхода ракетки за границы поля.
+            if (this.paddle.pos < 0) this.paddle.pos = 0;
+            if (this.paddle.pos + paddle_width > 1) this.paddle.pos = 1 - paddle_width;
+
             this.paddleMoved.raise()
         }
+
         if (!(this.ball.speed[0] === 0 && this.ball.speed[1] === 0)) {
+            //Ball size
+            var ball_width = $('div.ball').width()/$('#game').width();
+            var ball_height = $('div.ball').height()/$('#game').height();
+
             this.ball.pos[0] += this.ball.speed[0]*dt
+
+            //Exit the ball out of the field along the OX
+            if (this.ball.pos[0] < ball_width / 2) {
+                this.ball.pos[0] = ball_width / 2;
+                this.ball.speed[0] = -this.ball.speed[0];
+            }
+
+            if (this.ball.pos[0] + ball_width / 2 > 1) {
+                this.ball.pos[0] = 1 - ball_width / 2;
+                this.ball.speed[0] = -this.ball.speed[0];
+            }
+
             this.ball.pos[1] += this.ball.speed[1]*dt
+            //Exit the ball out of the field along the OY
+            if (this.ball.pos[1] - ball_height / 2 < 0) {
+                this.ball.pos[1] = ball_height / 2;
+                this.ball.speed[1] = -this.ball.speed[1];
+            }
+
             this.ballMoved.raise()
         }
-        //conatact ball and brick check and handling
-        checkContactBallBrick.call(this); 
+
+        //conatact ball adn brick check and handling
+        checkContactBallBrick.call(this);
+        checkContactBallPaddle.call(this);
     }
 
     function checkContactBallBrick()
@@ -110,6 +141,36 @@
     function distance(x1,y1,x2,y2)
     {
         return Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+    }
+    /**
+     * Обработка контакта ракетки с мячом.
+     */
+    function checkContactBallPaddle()
+    {
+        //Размеры мяча.
+        var ball_height = $('div.ball').height()/$('#game').height();
+
+        //Размеры ракетки.
+        var paddle_width =  $('.paddle').width() / $('#game').width();
+        var paddle_height =  $('.paddle').height() / $('#game').height();
+
+        //Если самая низшая точка ушла ниже самой высшей точки ракетки, то...
+        if (this.ball.pos[1] + ball_height / 2 >= 1 - paddle_height) {
+            //Границы ракетки по оси OX.
+            var paddle_left = this.paddle.pos;
+            var paddle_right = this.paddle.pos + paddle_width;
+
+            //Если мяч коснулся ракетки, то...
+            if (paddle_left <= this.ball.pos[0] && this.ball.pos[0] <= paddle_right) {
+                this.ball.speed[1] = -Math.abs(this.ball.speed[1]);
+            } /*Ball isn't touch paddle*/
+            else {
+                this.ball.speed[0] = Math.abs(this.ball.speed[0]);
+                this.ball.speed[1] = -Math.abs(this.ball.speed[1]);
+                this.ball.pos[0] = this.paddle.pos + paddle_width / 2;
+                this.ball.pos[1] = 1 - paddle_height - ball_height / 2;
+            }
+        }
     }
 
     arcanoid.GameState = GameState
